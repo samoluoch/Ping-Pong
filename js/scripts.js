3d699eb553ba26239c21b276fedb3bb95e49d647
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-07-13 11:56:23 +0300

    created a variable that will capture the converted input value after being changed to a number

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $jQuery(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    var input1 = $("input#input1").val();
-  })
+    var input1 = parseInt($("input#input1").val());
+    var result = numbers(input1);
+    $("#result").text(result);
+  });
+});
+var input1 = function(number){
+  
 }
